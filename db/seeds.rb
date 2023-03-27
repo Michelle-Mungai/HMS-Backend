f209630d3fc1f0593ddd54bf10541d1cb0bfd1e4
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-27 23:10:38 +0300

    Created User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(username: "johndoe", email: "johndoe@example.com", password: "password123", image_url: "https://example.com/johndoe.png", bio: "I'm a software engineer who loves to code!", created_at: Time.now, updated_at: Time.now)
+
+User.create(username: "janedoe", email: "janedoe@example.com", password: "password123", image_url: "https://example.com/janedoe.png", bio: "I'm a designer who loves to create beautiful things!", created_at: Time.now, updated_at: Time.now)
+
+User.create(username: "bobsmith", email: "bobsmith@example.com", password: "password123", image_url: "https://example.com/bobsmith.png", bio: "I'm a writer who loves to tell stories!", created_at: Time.now, updated_at: Time.now)
