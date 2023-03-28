b544be995af7e149709a89c90cd3ae73fb743cce
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-28 11:18:14 +0300

    Created doctors routes

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,3 +1,26 @@
 class DoctorsController < ApplicationController
+   # view doctors
+    def index
+        doctors= Doctor.all 
+        render json: doctors
+    end
+    
+    #creating a new doctor
+    def create
+        doctor = Doctor.new(doctor_params)
+      
+        if doctor.save
+          render json: doctor, status: :created, location: doctor
+        else
+          render json: doctor.errors, status: :unprocessable_entity
+        end
+      end
+      
+      private
+      
+      def doctor_params
+        params.require(:doctor).permit(:name, :phone_number, :medical_specialties, :medical_license_number)
+      end
+      
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :appointments
-  resources :doctors
+  resources :doctors, only: [:index, :create]
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
