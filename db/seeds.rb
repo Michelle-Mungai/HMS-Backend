57aaaf0d1b014274479e52bb05b7942f7af76543
Stiflerzak <hilarymwendia97@gmail.com>
2023-03-28 09:57:44 +0300

    Created Boilerplate

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-User.create(username: "johndoe", email: "johndoe@example.com", password: "password123", image_url: "https://example.com/johndoe.png", bio: "I'm a software engineer who loves to code!", created_at: Time.now, updated_at: Time.now)
-
-User.create(username: "janedoe", email: "janedoe@example.com", password: "password123", image_url: "https://example.com/janedoe.png", bio: "I'm a designer who loves to create beautiful things!", created_at: Time.now, updated_at: Time.now)
-
-User.create(username: "bobsmith", email: "bobsmith@example.com", password: "password123", image_url: "https://example.com/bobsmith.png", bio: "I'm a writer who loves to tell stories!", created_at: Time.now, updated_at: Time.now)
