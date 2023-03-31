9f648da5293b6dba2df7b90c165107557bb1993f
JacobMuchori <muskins18@gmail.com>
2023-03-31 17:37:25 +0300

    Uncommented password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,21 +20,21 @@ class UsersController < ApplicationController
         end
     end
 
-    def generate_password_reset_token
-        self.reset_password_token = SecureRandom.urlsafe_base64
-        self.reset_password_sent_at = Time.now.utc
-        save!
-      end
+    # def generate_password_reset_token
+    #     self.reset_password_token = SecureRandom.urlsafe_base64
+    #     self.reset_password_sent_at = Time.now.utc
+    #     save!
+    #   end
     
-      def reset_password_token_valid?
-        (self.reset_password_sent_at + 1.hour) > Time.now.utc
-      end
+    #   def reset_password_token_valid?
+    #     (self.reset_password_sent_at + 1.hour) > Time.now.utc
+    #   end
     
-      def clear_password_reset_token
-        self.reset_password_token = nil
-        self.reset_password_sent_at = nil
-        save!
-      end
+    #   def clear_password_reset_token
+    #     self.reset_password_token = nil
+    #     self.reset_password_sent_at = nil
+    #     save!
+    #   end
 
     private
 
